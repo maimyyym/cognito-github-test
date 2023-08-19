470120b75a8b88c7ef68f3e69e31fe44cf09af9a
maimy <m.711029@gmail.com>
2023-08-19 16:10:30 +0900

    format

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,20 +1,20 @@
 import NextAuth from "next-auth/next";
-import CognitoProvider from "next-auth/providers/cognito"
+import CognitoProvider from "next-auth/providers/cognito";
 
 export default NextAuth({
   providers: [
     CognitoProvider({
-        clientId: process.env.COGNITO_CLIENT_ID,
-        clientSecret: process.env.COGNITO_CLIENT_SECRET,
-        issuer: process.env.COGNITO_ISSUER,
+      clientId: process.env.COGNITO_CLIENT_ID,
+      clientSecret: process.env.COGNITO_CLIENT_SECRET,
+      issuer: process.env.COGNITO_ISSUER,
     }),
   ],
   pages: {
-    signIn: '/',
-    signOut: '/',
-    error: '/',
-    verifyRequest: '/',
-    newUser: '/' // or you can set a path
+    signIn: "/",
+    signOut: "/",
+    error: "/",
+    verifyRequest: "/",
+    newUser: "/", // or you can set a path
   },
   session: {
     jwt: true,
@@ -22,21 +22,21 @@ export default NextAuth({
   callbacks: {
     async jwt(token, user) {
       if (user) {
-        token.id = user.id
-        token.email = user.email
+        token.id = user.id;
+        token.email = user.email;
       }
-      return token
+      return token;
     },
     async session(session, token) {
-      session.user.id = token.id
-      return session
+      session.user.id = token.id;
+      return session;
     },
-  async redirect({ url, baseUrl }) {
-    // Allows relative callback URLs
-    if (url.startsWith("/")) return `${baseUrl}${url}`
-    // Allows callback URLs on the same origin
-    else if (new URL(url).origin === baseUrl) return url
-    return baseUrl
-  }
-}
-})
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,22 @@
-import Image from 'next/image'
-import { useSession, signIn, signOut } from "next-auth/react"
-
+import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
 
   if (session) {
     return (
-            <>
-             Signed in as {session.user?.name} <br />
+      <>
+        Signed in as {session.user?.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
-    ) } 
+    );
+  }
 
-    return (
-      <>
-            Not signed in <br />
+  return (
+    <>
+      Not signed in <br />
       <button onClick={() => signIn("github")}>Sign in</button>
-  </>
-
-  )
+    </>
+  );
 }
